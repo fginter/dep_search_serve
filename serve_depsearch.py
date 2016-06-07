f3b3cdfe32b82182d76f5a97709be84d7b50e80e
Filip Ginter <ginter@cs.utu.fi>
2016-06-07 16:42:44 +0300

    latest changes

--- serve_depsearch.py
+++ serve_depsearch.py
@@ -10,15 +10,25 @@ except ImportError:
 
 app = Flask("dep_search_webapp")
 
+def yield_trees(src):
+    current_tree=[]
+    for line in src:
+        current_tree.append(line)
+        if line=="":
+            yield "\n".join(current_tree)
+            current_tree=[]
+
 @app.route("/")
 def index():
     return flask.render_template("index_template.html")
 
 @app.route('/query',methods=["POST"])
-def nearest():
+def query():
     query=flask.request.form['query'].strip()
-    hits_per_page=int(flask.request.form['hits_per_page'])
-    ret=flask.render_template("result_tbl.html",query=query)
+    hits_per_page=3#int(flask.request.form['hits_per_page'])
+    
+    r=requests.get("http://bionlp-www.utu.fi/dep_search_api",params={"db":"Finnish", "search":query, "retmax":hits_per_page},stream=True)
+    ret=flask.render_template("result_tbl.html",trees=yield_trees(l.decode("utf-8") for l in r.iter_lines()))
     return json.dumps({'ret':ret});
 
 if __name__ == '__main__':
