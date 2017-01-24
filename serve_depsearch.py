6260fe1884bff218dda80105fa85bb836fcfce2c
Filip Ginter <ginter@cs.utu.fi>
2017-01-24 10:14:48 +0000

    Python2 compatibility

--- serve_depsearch.py
+++ serve_depsearch.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python3
 
+# This code can run in both Python 2.7+ and 3.3+
+
 from flask import Flask, Markup
 import flask
 import json
 import requests
-import urllib
+import six
+import six.moves.urllib as urllib  # use six for python 2.x compatibility
+import traceback
 
-DEBUGMODE=True
+DEBUGMODE=False
 try:
     from config_local import * #use to override the constants above if you like
 except ImportError:
@@ -17,36 +21,36 @@ app = Flask("dep_search_webgui")
 def yield_trees(src):
     current_tree=[]
     current_comment=[]
-    current_context=""
+    current_context=u""
     for line in src:
-        if line.startswith("# visual-style"):
+        if line.startswith(u"# visual-style"):
             current_tree.append(line)
-        elif line.startswith("# URL:"):
-            current_comment.append(Markup('<a href="{link}">{link}</a>'.format(link=line.split(":",1)[1].strip())))
-        elif line.startswith("# context-hit"):
-            current_context+=(' <b>{sent}</b>'.format(sent=flask.escape(line.split(":",1)[1].strip())))
-        elif line.startswith("# context"):
-            current_context+=(' {sent}'.format(sent=flask.escape(line.split(":",1)[1].strip())))
-        elif line.startswith("# hittoken"):
+        elif line.startswith(u"# URL:"):
+            current_comment.append(Markup(u'<a href="{link}">{link}</a>'.format(link=line.split(u":",1)[1].strip())))
+        elif line.startswith(u"# context-hit"):
+            current_context+=(u' <b>{sent}</b>'.format(sent=flask.escape(line.split(u":",1)[1].strip())))
+        elif line.startswith(u"# context"):
+            current_context+=(u' {sent}'.format(sent=flask.escape(line.split(u":",1)[1].strip())))
+        elif line.startswith(u"# hittoken"):
             current_tree.append(line)
-        elif not line.startswith("#"):
+        elif not line.startswith(u"#"):
             current_tree.append(line)
-        if line=="":
+        if line==u"":
             current_comment.append(Markup(current_context))
-            yield "\n".join(current_tree), current_comment
+            yield u"\n".join(current_tree), current_comment
             current_comment=[]
             current_tree=[]
-            current_context=""
+            current_context=u""
 
 
 class Query:
 
     @classmethod
     def from_formdata(cls,fdata):
-        query=fdata['query'].strip()
-        hits_per_page=int(fdata['hits_per_page'])
-        treeset=fdata['treeset'].strip()
-        if fdata.get('case'):
+        query=fdata[u'query'].strip()
+        hits_per_page=int(fdata[u'hits_per_page'])
+        treeset=fdata[u'treeset'].strip()
+        if fdata.get(u'case'):
             case_sensitive=True
         else:
             case_sensitive=False
@@ -54,8 +58,8 @@ class Query:
 
     @classmethod
     def from_get_request(cls,args):
-        query=args["search"]
-        treeset=args["db"]
+        query=args[u"search"]
+        treeset=args[u"db"]
         case_sensitive=True
         hits_per_page=10
         return(cls(treeset,query,case_sensitive,hits_per_page))
@@ -63,49 +67,60 @@ class Query:
     def __init__(self,treeset,query,case_sensitive,hits_per_page):
         self.treeset,self.query,self.case_sensitive,self.hits_per_page=treeset,query,case_sensitive,hits_per_page
 
-    def query_link(self,url=""):
-        return url+"query?search={query}&db={treeset}&case_sensitive={case_sensitive}&hits_per_page={hits_per_page}".format(query=urllib.parse.quote(self.query),treeset=self.treeset,case_sensitive=self.case_sensitive,hits_per_page=self.hits_per_page)
+    def query_link(self,url=u""):
+        if six.PY2:
+            return url+u"query?search={query}&db={treeset}&case_sensitive={case_sensitive}&hits_per_page={hits_per_page}".format(query=unicode(urllib.parse.quote(self.query.encode("utf-8")),"utf-8"),treeset=self.treeset,case_sensitive=self.case_sensitive,hits_per_page=self.hits_per_page)
+        else:
+            return url+u"query?search={query}&db={treeset}&case_sensitive={case_sensitive}&hits_per_page={hits_per_page}".format(query=urllib.parse.quote(self.query),treeset=self.treeset,case_sensitive=self.case_sensitive,hits_per_page=self.hits_per_page)
 
     def download_link(self,url=""):
-        return DEP_SEARCH_WEBAPI+"?search={query}&db={treeset}&case={case_sensitive}&retmax=5000&dl".format(query=urllib.parse.quote(self.query),treeset=self.treeset,case_sensitive=self.case_sensitive)
+        if six.PY2:
+            return DEP_SEARCH_WEBAPI+u"?search={query}&db={treeset}&case={case_sensitive}&retmax=5000&dl".format(query=unicode(urllib.parse.quote(self.query.encode("utf-8")),"utf-8"),treeset=self.treeset,case_sensitive=self.case_sensitive)
+        else:
+            return DEP_SEARCH_WEBAPI+u"?search={query}&db={treeset}&case={case_sensitive}&retmax=5000&dl".format(query=urllib.parse.quote(self.query),treeset=self.treeset,case_sensitive=self.case_sensitive)
         
-@app.route("/")
+@app.route(u"/")
 def index():
-    r=requests.get(DEP_SEARCH_WEBAPI+"/metadata") #Ask about the available corpora
+    r=requests.get(DEP_SEARCH_WEBAPI+u"/metadata") #Ask about the available corpora
     metadata=json.loads(r.text)
-    return flask.render_template("index_template.html",corpus_groups=metadata["corpus_groups"])
+    return flask.render_template(u"index_template.html",corpus_groups=metadata[u"corpus_groups"])
 
 #This is what JS+AJAX call
-@app.route('/query',methods=["POST"])
+@app.route(u'/query',methods=[u"POST"])
 def query_post():
-    q=Query.from_formdata(flask.request.form)
-    r=requests.get(DEP_SEARCH_WEBAPI,params={"db":q.treeset, "case":q.case_sensitive, "context":3, "search":q.query, "retmax":q.hits_per_page})
-    if r.text.startswith("# Error in query"):
-        ret = flask.render_template("query_error.html", err=r.text)
-    elif not r.text.strip():
-        ret = flask.render_template("empty_result.html")
-    else:
-        ret=flask.render_template("result_tbl.html",trees=yield_trees(r.text.splitlines()))
-    return json.dumps({'ret':ret,'query_link':q.query_link(),'download_link':q.download_link()});
+    try:
+        q=Query.from_formdata(flask.request.form)
+        r=requests.get(DEP_SEARCH_WEBAPI,params={u"db":q.treeset, u"case":q.case_sensitive, u"context":3, u"search":q.query, u"retmax":q.hits_per_page})
+        if r.text.startswith(u"# Error in query"):
+            ret = flask.render_template(u"query_error.html", err=r.text)
+        elif not r.text.strip():
+            ret = flask.render_template(u"empty_result.html")
+        else:
+            ret=flask.render_template(u"result_tbl.html",trees=yield_trees(r.text.splitlines()))
+
+        return json.dumps({u'ret':ret,u'query_link':q.query_link(),u'download_link':q.download_link()});
+    except:
+        traceback.print_exc()
+        
 
 #This is what GET calls
 #We return the index and prefill a script call to launch the form for us
-@app.route('/query',methods=["GET"])
+@app.route(u'/query',methods=[u"GET"])
 def query_get():
-    r=requests.get(DEP_SEARCH_WEBAPI+"/metadata") #Ask about the available corpora
+    r=requests.get(DEP_SEARCH_WEBAPI+u"/metadata") #Ask about the available corpora
     metadata=json.loads(r.text)
 
-    if "db" not in flask.request.args or "search" not in flask.request.args:
-        return flask.render_template("get_help.html",corpus_groups=metadata["corpus_groups"])
+    if u"db" not in flask.request.args or u"search" not in flask.request.args:
+        return flask.render_template(u"get_help.html",corpus_groups=metadata[u"corpus_groups"])
 
     q=Query.from_get_request(flask.request.args)
-    run_request=Markup('dsearch_simulate_form("{treeset}","{query}","{case_sensitive}","{max_hits}");'.format(treeset=q.treeset,query=q.query.replace('"',r'\"'),case_sensitive=q.case_sensitive,max_hits=q.hits_per_page))
-    return flask.render_template("index_template.html",corpus_groups=metadata["corpus_groups"],run_request=run_request)
+    run_request=Markup(u'dsearch_simulate_form("{treeset}","{query}","{case_sensitive}","{max_hits}");'.format(treeset=q.treeset,query=q.query.replace(u'"',ur'\"'),case_sensitive=q.case_sensitive,max_hits=q.hits_per_page))
+    return flask.render_template(u"index_template.html",corpus_groups=metadata[u"corpus_groups"],run_request=run_request)
     
 
 
-if __name__ == '__main__':
+if __name__ == u'__main__':
     app.run(debug=DEBUGMODE)
-    r=requests.get(DEP_SEARCH_WEBAPI+"/metadata") #Ask about the available corpora
+    r=requests.get(DEP_SEARCH_WEBAPI+u"/metadata") #Ask about the available corpora
     metadata=json.loads(r.text)
 
